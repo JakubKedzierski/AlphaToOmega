49d1d0e53c106a7347c9ff0338d6ffd85e3672ea
Jakub Kedzierski <248915@student.pwr.edu.pl>
2021-01-31 13:23:21 +0100

    listening thread conception + started work on client handler

--- pictionary/src/main/java/pictionary/PictionaryServer.java
+++ pictionary/src/main/java/pictionary/PictionaryServer.java
@@ -1,16 +1,25 @@
 package pictionary;
 
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class PictionaryServer implements Runnable {
 	public static int SERVER_PORT=25000;
-	Map<ClientHandler, Integer> map=new HashMap<ClientHandler, Integer>();
-	int userCount=0;
+	private List<ClientHandler> users=new ArrayList<ClientHandler>();
+	private int userCount=0;
 	
 	
 	PictionaryServer(){
@@ -21,6 +30,15 @@ public class PictionaryServer implements Runnable {
 	public static void main(String[] args) {
 		new PictionaryServer();
 
+	}
+	
+	private ClientHandler getClientHandlerById(final String id) {
+		for(ClientHandler handler:users) {
+			if(handler.getUserId()==id) {
+				return handler;
+			}
+		}
+		return null;
 	}
 
 
@@ -34,7 +52,9 @@ public class PictionaryServer implements Runnable {
 			while(userCount<4) {
 				Socket clientSocket = serverSocket.accept();
 				if(clientSocket!=null) {
-					newConnectionStartup(clientSocket);
+					ClientHandler userHandler=new ClientHandler(this,clientSocket);
+					users.add(userHandler);
+					userCount++;
 				}
 				
 			}
@@ -46,20 +66,84 @@ public class PictionaryServer implements Runnable {
 		}
 		
 	}
-		
-	private void newConnectionStartup(Socket clientSocket) {
-		
-	}
-	
-
-}
 
 
-class ClientHandler{
-	int userId;
-	
-	ClientHandler(){
-		
+	public void removeHandler(ClientHandler clientHandler) {
+		users.remove(clientHandler);
+		userCount--;
 	}
 	
 }
+
+class ClientHandler implements Runnable{
+	
+	private String userId=null;
+	private PictionaryServer server=null;
+	private Socket socket=null;
+	private ObjectInputStream inputStream=null;
+	private ObjectOutputStream outputStream=null;
+	
+	ClientHandler(PictionaryServer server,Socket socket){
+		this.server=server;
+		this.socket=socket;
+		new Thread(this).start();
+	}
+
+	@Override
+	public void run() {
+		try (ObjectOutputStream output = new ObjectOutputStream(socket.getOutputStream());
+				ObjectInputStream input = new ObjectInputStream(socket.getInputStream());) {
+			
+			outputStream = output;
+			inputStream = input;
+			
+			String message=(String)input.readObject();
+			
+			if(message!=null) {
+				try{
+					newConnectionStartup(message);
+				}catch(IOException fatalException) {
+					server.removeHandler(this);
+					return;
+				}
+			}
+			
+			while(true) {
+
+			}
+			
+		}catch(Exception exception) {
+			
+		}
+		
+	}
+
+	private void newConnectionStartup(String message) throws IOException{
+
+	    ObjectMapper mapper = new ObjectMapper();
+
+			JsonNode greetingMessage = mapper.readTree(message);
+			if (greetingMessage.has("name")) {
+			    userId=greetingMessage.path("name").asText();
+			}else {
+				throw new IOException("Message dosent contain name attribute.");
+			}
+
+	}
+	
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	
+	@Override
+	public String toString() {
+		return "Handler of" + userId + "\n";
+		
+	}
+	
+}
+
